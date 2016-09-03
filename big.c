c89bbd726e28b5a8876c6ba6f4eaf9cc7f49a000
Aishwarya1196 <aishwaryamohan1196@gmail.com>
2016-09-03 11:45:13 +0530

    Update and rename vowel.c to big.c

---
+++ big.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  int a,b,c,big;
+   printf("enter three nos:");
+  scanf("%d%d%d"&a&b&c);
+  if(a>b)&&(a>c)
+  {
+    big=a;
+    else if(b>c)
+    {
+      big=b;
+    }
+      else
+      {
+        big=c;
+      }
+    }
+    getch();
+  }
+
